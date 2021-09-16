9f4c1b31289cdfc0b362eacdf3cd2980cad73df6
Panachai <panachai.ny@gmail.com>
2021-09-16 16:22:18 +0700

    Update GTAG.vue

--- ui/src/components/GTAG.vue
+++ ui/src/components/GTAG.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <button v-on:click="gtmSend">GTM TEST</button>
+    <button id="gtm-test-btn" v-on:click="gtmSend">GTM TEST</button>
   </div>
 </template>
 
@@ -11,6 +11,16 @@ export default {
   methods: {
     gtmSend: function() {
       console.log("gtmSend ~");
+      console.log("this.$gtm.enabled() ->", this.$gtm.enabled());
+
+      this.$gtm.trackEvent({
+        event: 'event', // Event type [default = 'interaction'] (Optional)
+        category: "Calculator",
+        action: "click",
+        label: "Home page SIP calculator",
+        value: 5000,
+        noninteraction: false, // Optional
+      });
     },
   },
 };
