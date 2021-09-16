3e2d46c0afec7bff9d44e4d7382839933b80a481
Panachai <panachai.ny@gmail.com>
2021-09-16 14:58:00 +0700

    add GTM + GTAG

--- ui/src/main.js
+++ ui/src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueGtag, {
+  config: { id: process.env.GOOGLE_ANALYTICS_ID }
+});
+
+Vue.use(VueGtm, {
+  id: process.env.GOOGLE_TAG_MANAGER,
+  defer: false,
+  compatibility: false,
+  enabled: true,
+  debug: true,
+  loadScript: true,
+  trackOnNextTick: false,
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
