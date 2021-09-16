3318dc2cc5f72b8c6708a82a6e0c993840c119b5
Panachai <panachai.ny@gmail.com>
2021-09-16 15:45:46 +0700

    init GTAG component

---
+++ ui/src/components/GTAG.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <button v-on:click="gtmSend">GTM TEST</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  methods: {
+    gtmSend: function() {
+      console.log("gtmSend ~");
+    },
+  },
+};
+</script>
