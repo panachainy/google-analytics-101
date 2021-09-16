47b584af2f9d7660ebcbad6533f37c4479a82a14
Panachai <panachai.ny@gmail.com>
2021-09-17 00:38:11 +0700

    Update GTAG.vue

--- ui/src/components/GTAG.vue
+++ ui/src/components/GTAG.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <button id="gtm-test-btn-1" v-on:click="gtmSend">GTM TEST 1</button>
+    <!-- <button id="gtm-test-btn-1" v-on:click="gtmSend">GTM TEST 1</button> -->
     <button id="gtm-test-btn-2">GTM TEST 2</button>
   </div>
 </template>
@@ -10,19 +10,19 @@ export default {
   name: "HelloWorld",
 
   methods: {
-    gtmSend: function() {
-      console.log("gtmSend ~");
-      console.log("this.$gtm.enabled() ->", this.$gtm.enabled());
+    // gtmSend: function() {
+    //   console.log("gtmSend ~");
+    //   console.log("this.$gtm.enabled() ->", this.$gtm.enabled());
 
-      this.$gtm.trackEvent({
-        event: "event", // Event type [default = 'interaction'] (Optional)
-        category: "Calculator",
-        action: "click",
-        label: "Home page SIP calculator",
-        value: 5000,
-        noninteraction: false, // Optional
-      });
-    },
+    //   this.$gtm.trackEvent({
+    //     event: "event", // Event type [default = 'interaction'] (Optional)
+    //     category: "Calculator",
+    //     action: "click",
+    //     label: "Home page SIP calculator",
+    //     value: 5000,
+    //     noninteraction: false, // Optional
+    //   });
+    // },
   },
 };
 </script>
